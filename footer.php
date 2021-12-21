444a27e8258371070167d7c2932251685e454ca2
Fabarm <87820977+Fabarm@users.noreply.github.com>
2021-12-21 17:50:14 +0200

    add site layout

---
+++ footer.php
@@ -0,0 +1,33 @@
+<footer class="footer">
+        <div class="container">
+            <div class="footer__inner">
+                <a href = "#" class = "logo">
+                    <img src = "images/logo.svg" alt = "" class = "logo__img">
+                </a>
+                <div class="social footer__social">
+                    <a class="social-link" href = "#">
+                        <img src="images/instagram.svg" alt = "instagram-icon" class = "social-img">
+                    </a>
+                    <a class="social-link" href = "#">
+                        <img src = "images/telegram.svg" alt = "telegram-icon" class = "social-img">
+                    </a>
+                    <a class="social-link" href = "#">
+                        <img src = "images/whatsapp.svg" alt = "whatsapp-icon" class = "social-img">
+                    </a>
+                    <a class="social-link" href = "#">
+                        <img src = "images/facebook.svg" alt = "facebook-icon" class = "social-img">
+                    </a>
+                </div>
+                <a href="#" class="footer__copy">
+                    Политика конфиденциальности
+                </a>
+            </div>
+        </div>
+    </footer>
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="js/slick.min.js"></script>
+    <script src="js/jquery.fancybox.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+</html>    
---
+++ header.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>AUTO from USA</title>
+
+    <?php wp_head(); ?>
+
+    <!-- <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/slick.css">
+    <link rel="stylesheet" href="css/style.css"> -->
+
+</head>
+<body>
+    <header class="header">
+        <div class="container">
+            <div class="header__top">
+                <a href = "#" class = "logo">
+                    <img src = "images/logo.svg" alt = "" class = "logo__img">
+                </a>
+                <a href = "tel:+380505556677" class = "phone">+38 (050) 555 66 77</a>
+            </div>
+            <div class="header__content">
+                <h1 class="header__title">авто из сша “под ключ”</h1>
+                <h2 class="header__subtitle">ЗАКАЖИ СЕБЕ АВТО ИЗ США С ВЫГОДОЙ ДО 40%</h2>
+                <p class="header__text">Подбор, покупка, доставка, растаможка, ремонт, оформление по договору</p>
+                <a href="#" class="button">КОНСУЛЬТАЦИЯ ЭКСПЕРТА</a>
+                <div class="social header__social">
+                    <a class="social-link" href = "#">
+                        <img src="images/instagram.svg" alt = "instagram-icon" class = "social-img">
+                    </a>
+                    <a class="social-link" href = "#">
+                        <img src = "images/telegram.svg" alt = "telegram-icon" class = "social-img">
+                    </a>
+                    <a class="social-link" href = "#">
+                        <img src = "images/whatsapp.svg" alt = "whatsapp-icon" class = "social-img">
+                    </a>
+                    <a class="social-link" href = "#">
+                        <img src = "images/facebook.svg" alt = "facebook-icon" class = "social-img">
+                    </a>
+                </div>
+                <img class="header__images" src = "images/ford-mustang.png" alt = "ford-mustang" >
+            </div>
+        </div>
+    </header>
